9d3cef276b4d75657572c68403c2b3685ddd31a4
Luciano Silva <lcnsilvajf@gmail.com>
2024-11-02 00:49:53 -0300

    fix: updated api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL:'http://localhost:10000/'
-})
 
+const api_url = 'https://teste-fatto-api.onrender.com/'
+const api = axios.create({
+    baseURL: api_url || 'http://localhost:10000/'
+})
 export default api
